383363ba696a6db74386c7eabe5cc9ba15698efa
Thomas Michiels <thomasmichiels0@gmail.com>
2025-04-16 13:10:57 +0200

    chat endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.api import chat
 
 app = FastAPI()
 
@@ -11,3 +12,5 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+app.include_router(chat.router, prefix="/api")
